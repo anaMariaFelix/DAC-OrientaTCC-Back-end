45596334e0b9f4e8e1c754bed3ae4a083039e3b1
user.name <anafelix0909@gmail.com>
2025-07-28 00:21:08 -0300

    refactor: desfazendo comentarios nos preAuthorize dos endpoints

--- src/main/java/dac/orientaTCC/controller/AlunoController.java
+++ src/main/java/dac/orientaTCC/controller/AlunoController.java
@@ -2,6 +2,7 @@ package dac.orientaTCC.controller;
 
 import java.util.List;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -22,6 +23,7 @@ import dac.orientaTCC.service.AlunoService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
+@Slf4j
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("api/alunos")
@@ -30,50 +32,52 @@ public class AlunoController {
     private final AlunoService alunoService;
 
     @PostMapping("/")
-    //@PreAuthorize("hasRole('COORDENADOR')")
+    @PreAuthorize("hasRole('COORDENADOR')")
     public ResponseEntity<AlunoResponseDTO> create(@RequestBody @Valid AlunoCreateDTO alunoCreateDTO){
+        log.info("Controller create aluno log 1");
         AlunoResponseDTO aluno = alunoService.create(alunoCreateDTO);
-
+        log.info("Controller create aluno log 2");
         return ResponseEntity.status(HttpStatus.CREATED).body(aluno);
     }
 
-    //@PreAuthorize("hasRole('ALUNO')")
+    @PreAuthorize("hasRole('ALUNO')")
     @GetMapping("/{id}")
     public ResponseEntity<AlunoResponseDTO> findById(@PathVariable Long id){
         Aluno aluno = alunoService.findById(id);
         return ResponseEntity.ok().body(AlunoMapper.toAlunoDTO(aluno));
     }
 
-    //@PreAuthorize("hasRole('COORDENADOR') OR #email == authentication.principal.username")
+    @PreAuthorize("hasRole('COORDENADOR') OR #email == authentication.principal.username")
     @GetMapping("/email/{email}")
     public ResponseEntity<AlunoResponseDTO> findByEmail(@PathVariable String email){
         Aluno aluno = alunoService.findByEmail(email);
         return ResponseEntity.ok(AlunoMapper.toAlunoDTO(aluno));
     }
 
-    //@PreAuthorize("hasRole('COORDENADOR') OR #matricula == authentication.principal.identificador")
+    @PreAuthorize("hasRole('COORDENADOR') OR #matricula == authentication.principal.identificador")
     @GetMapping("/matricula/{matricula}")
     public ResponseEntity<AlunoResponseDTO> findByMatricula(@PathVariable String matricula){
 
         Aluno aluno = alunoService.findByMatricula(matricula);
+        log.info("matricula",aluno.getMatricula());
         return ResponseEntity.ok(AlunoMapper.toAlunoDTO(aluno));
     }
 
-    //@PreAuthorize("hasRole('COORDENADOR')")
+    @PreAuthorize("hasRole('COORDENADOR')")
     @GetMapping("/")
     public ResponseEntity<List<AlunoResponseDTO>> findAll(){
         List<Aluno> alunos = alunoService.findAll();
         return ResponseEntity.ok(AlunoMapper.toListAlunoDTO(alunos));
     }
 
-    //@PreAuthorize("hasRole('COORDENADOR')")
+    @PreAuthorize("hasRole('COORDENADOR')")
     @DeleteMapping("/email/{email}")
     public ResponseEntity<Void> removeByEmail(@PathVariable String email){
         alunoService.remove(email);
         return ResponseEntity.noContent().build();
     }
 
-    //@PreAuthorize("hasRole('ALUNO') AND #alunoCreateDTO.getMatricula == authentication.principal.identificador")
+    @PreAuthorize("hasRole('ALUNO') AND #alunoCreateDTO.getMatricula == authentication.principal.identificador")
     @PutMapping("/")
     public ResponseEntity<AlunoResponseDTO> update(@RequestBody AlunoCreateDTO alunoCreateDTO){
         Aluno aluno = alunoService.update(alunoCreateDTO);
