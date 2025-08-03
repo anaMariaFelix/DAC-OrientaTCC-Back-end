02dba6ab72293db79a8d500cb44099e5ad98d703
user.name <anafelix0909@gmail.com>
2025-08-03 18:05:45 -0300

    refactor: refatoração da classe atividadeMapper

--- src/main/java/dac/orientaTCC/mapper/AtividadeMapper.java
+++ src/main/java/dac/orientaTCC/mapper/AtividadeMapper.java
@@ -1,46 +1,15 @@
 package dac.orientaTCC.mapper;
 
-import java.util.List;
-import java.util.stream.Collectors;
-
+import org.modelmapper.ModelMapper;
 import dac.orientaTCC.dto.AtividadeDTO;
-import dac.orientaTCC.dto.PdfDTO;
 import dac.orientaTCC.model.entities.Atividade;
-import dac.orientaTCC.model.entities.PDF;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class AtividadeMapper {
-	
-	public static Atividade atividadeMapper(AtividadeDTO atividadeDTO) {
-		Atividade atividade = new Atividade();
-		
-		atividade.setComentario(atividadeDTO.getComentarios());
-		atividade.setDataEntrega(atividadeDTO.getDataEntrega());
-		atividade.setDescricao(atividadeDTO.getDescricao());
-		atividade.setNome(atividadeDTO.getNome());
-		atividade.setStatus(atividadeDTO.getStatusPdf());
-		//atividade.set(atividadeDTO.getTrabalhoAcademico());
-		
-		// Aqui faz a conversão de PdfDTO para PDF (se a lista não for nula)
-        if (atividadeDTO.getPdfs() != null) {
-            List<PDF> pdfs = atividadeDTO.getPdfs().stream()
-                .map(AtividadeMapper::pdfDtoToEntity)
-                .collect(Collectors.toList());
 
-            atividade.setPdfs(pdfs);
-        }
-
-		
-		return atividade;
-	}
-	
-	// Conversão de PdfDTO para PDF
-    public static PDF pdfDtoToEntity(PdfDTO pdfDTO) {
-        if (pdfDTO == null) return null;
-
-        PDF pdf = new PDF();
-        pdf.setNomeArquivo(pdfDTO.getNomeArquivo());
-        pdf.setConteudo(pdfDTO.getConteudo());
-
-        return pdf;
+    public static Atividade toAtividade(AtividadeDTO atividadeDTO){
+        return new ModelMapper().map(atividadeDTO, Atividade.class);
     }
 }
