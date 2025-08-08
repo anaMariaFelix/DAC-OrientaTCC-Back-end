8d7c800ca2859ad1526b98ca29e0734a0889d0ea
user.name <anafelix0909@gmail.com>
2025-08-07 21:34:10 -0300

    refactor: apagando comentarios da classe SpringSecurityConfig

--- src/main/java/dac/orientaTCC/config/SpringSecurityConfig.java
+++ src/main/java/dac/orientaTCC/config/SpringSecurityConfig.java
@@ -20,46 +20,41 @@ import static org.springframework.security.config.Customizer.withDefaults;
 
 
 @EnableMethodSecurity
-@EnableWebMvc // NECESSARIA PARA TRABALHAR COM SISTEMA DE SEGURAÇÃO
+@EnableWebMvc
 @Configuration
 public class SpringSecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http
-                .cors(withDefaults()) // Ativa o CORS com a configuração acima
-                .csrf(csrf -> csrf.disable())//desabilitamdo essas funções para trabalhar com APIREST usando a forma stateless
+                .cors(withDefaults())
+                .csrf(csrf -> csrf.disable())
                 .formLogin(form -> form.disable())
                 .httpBasic(basic -> basic.disable())
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(HttpMethod.POST, "/api/auth").permitAll()//libera o acesso de forma publica para todos os usuarios que queiram realizar a autenticação na api precisam de autenticação para serem utilizadas
+                        .requestMatchers(HttpMethod.POST, "/api/auth").permitAll()
                         .anyRequest().authenticated()
                 ).sessionManagement(
-                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)//defini a politica de seção, como a aplicação é do tipo Ret a politica usada é a stateless
+                        session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 ).addFilterBefore(
-                        jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class //adiciona a ordem de execução dos filtros, primiero execulta o nosso filtro jwtAuthorizationFilter e so depois o UsernamePasswordAuthenticationFilter, se  aordem for inversa não teria o resultado esperado
+                        jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class
                 ).exceptionHandling(ex -> ex
-                        .authenticationEntryPoint(new JwtAuthenticationEntryPoint())//instancia da classe que foi criada no pacakage jwt que lança uma exeção caso o usuario não esteja autenticado, dessa forma se o usuario n estiver logado a exeção ser lançada
+                        .authenticationEntryPoint(new JwtAuthenticationEntryPoint())
                 ).build();
     }
 
-    @Bean // referente ao tipo de criptografia de senhas
+    @Bean
     public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(); // classe referente ao tipo de criptografia que sera utilizada q é a
-                                            // BCrypt atualmente ela é considerada a criptografia mais segura,
-                                            // usando ela a cada vez q uma senha é criptografada é criada uma
-                                            // nova criptografia dIferente para a senha,
-                                            // ou seja msm sendo a msm senha a criptofrafia dela sempre sera
-                                            // diferente a cada criptografia
+        return new BCryptPasswordEncoder();
     }
 
-    @Bean // referente ao gerenciamento de autenticação
+    @Bean
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration)
             throws Exception {
-        return authenticationConfiguration.getAuthenticationManager(); // retorna um objeto de gerenciamento de autenticação
+        return authenticationConfiguration.getAuthenticationManager();
     }
 
-    @Bean // bean para colocar a classe filter sobre o gerenciamento do spring
+    @Bean
     public JwtAuthorizationFilter jwtAuthorizationFilter() {
         return new JwtAuthorizationFilter();
     }
