7a2bccbc74cc059bc86351f3cfbdabb4474a943a
user.name <anafelix0909@gmail.com>
2025-08-07 21:35:24 -0300

    refactor: apagando comentarios da classe JwtAuthorizationFilter

--- src/main/java/dac/orientaTCC/jwt/JwtAuthorizationFilter.java
+++ src/main/java/dac/orientaTCC/jwt/JwtAuthorizationFilter.java
@@ -19,43 +19,38 @@ public class JwtAuthorizationFilter extends OncePerRequestFilter {
     @Autowired
     private JwtUserDetailsService userDetailsService;
 
-    @Override  //metodo que intercepta as requisições, herdado da classe extendida
+    @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        final String token = request.getHeader(JwtUtils.JWT_AUTHORIZATION); //recebe uma string como parametro que sera a string do cabeçario
+        final String token = request.getHeader(JwtUtils.JWT_AUTHORIZATION);
 
-        if(token == null || !token.startsWith(JwtUtils.JWT_BEARER)){ //startsWith testa a parte inicial de uma string
+        if(token == null || !token.startsWith(JwtUtils.JWT_BEARER)){
             log.info("JWT Token esta nulo, vazio ou não iniciado com 'Bearer'.");
             filterChain.doFilter(request,response);
-            //as requisções que não precisam de um token ficam po esse if, como por exemplo as de criação de um usuario ou de autenticação na aplicaçao
+
             return;
         }
 
-        //caso exita um token e ele não esta valido cai nesse if
         if(!JwtUtils.isTokenValid(token)){
             log.warn("JWT Token está invalido ou expirado");
             filterChain.doFilter(request,response);
             return;
         }
 
-        //caso exista um token e ele seja valido
-        String username = JwtUtils.getEmailFromTokem(token); //pega o username do token valido
+        String username = JwtUtils.getEmailFromTokem(token);
 
         toAuthentication(request, username);
-        filterChain.doFilter(request, response); //se chegar nessa linha significa que o spring finalizou a parte de authenticação pelo token e assim o usuario vai conseguir acessar o metodo que ele esta tentando acessar com a requição solicitada
+        filterChain.doFilter(request, response);
     }
 
     private void toAuthentication(HttpServletRequest request, String email) {
 
-        //pega o email para fazer a consulta pelo o usuario no banco de dados
         UserDetails userDetails = userDetailsService.loadUserByUsername(email);
 
-        //caso exista um usuario e de tudo certo
         UsernamePasswordAuthenticationToken authenticationToken = UsernamePasswordAuthenticationToken
                 .authenticated(userDetails, null, userDetails.getAuthorities());
 
-        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request)); // a passagem do request que é o objeto de requisição,
-                                                                                                    // para a parte de authenticação do spring security
-                                                                                                    // e assim ele consegue unir as operações que é a parte de segurança com as operações da requisição
-        SecurityContextHolder.getContext().setAuthentication(authenticationToken); //o authenticationToken com todas as informações necessarias para realizar a authenticação desse usuario
+        authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+        SecurityContextHolder.getContext().setAuthentication(authenticationToken);
     }
 }
